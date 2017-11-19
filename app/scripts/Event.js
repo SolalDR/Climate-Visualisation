0b6dad3e60e21fb9b837364918dad66f28dc4658
SolalDR <solal.dussout-revel@hotmail.fr>
2017-11-19 22:57:22 +0100

    Add event class

---
+++ app/scripts/Event.js
@@ -0,0 +1,43 @@
+class Event {
+
+  eventExist(event, callback){
+    var exist = false; 
+    if( this.events[event] ) {
+      for(i=0; i<this.events[event].length; i++){
+        if( this.events[event] == callback ){
+          exist = true; 
+        }
+      }
+    }
+    return exist; 
+  }
+
+  dispatch(event){
+    if( this.events[event] && this.events[event].length ){
+      for(i=0; i<this.events[event].length; i++ ){
+        var callback = this.events[event][i];
+        callback.call(this);
+      }
+    }
+  }
+
+  on(event, callback){
+    if( !this.events[event] && parseInt(this.eventsList.indexOf(event)) ) {
+      this.events[ event ] = [];
+    }
+    if( this.events[event] && !this.eventExist(event, callback) ) {
+      this.events[event].push(callback);
+    }
+  }
+
+
+  off(event, callback){
+    if( this.events[event] ){
+      for(i=0; i<this.events[event].length; i++){
+        if( this.events[event][i] == callback ){
+          this.events[event].slice(i, 1);
+        }
+      }
+    }
+  }
+}
